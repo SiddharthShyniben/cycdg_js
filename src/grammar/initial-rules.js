9a2177bc1a176c5fd6e87e5a977f7dbb12ccf62d
Siddharth Shyniben <siddharth.muscat@gmail.com>
2024-05-28 11:06:09 +0530

    Add initial rules

---
+++ src/grammar/initial-rules.js
@@ -0,0 +1,159 @@
+import { add, adjacent, is, manhattan, vec } from "../coords";
+import {
+  coordsInGraphBounds,
+  drawBiconnectedDirectionalRect,
+  graphAddEdgeTagByCoords,
+  graphAddNodeTag,
+  graphAddNodeTagPreserveLastId,
+  graphNodeHasTag,
+  graphSize,
+  tags,
+} from "../graph";
+import {
+  areCoordsOnRect,
+  getRandomGraphCoordsByFunc,
+  pushNodeContensInRandomDirection,
+  pushNodeContensInRandomDirectionWithEdgeTag,
+  randomHazard,
+  randomInRange,
+} from "../util";
+
+export default [
+  rule({
+    name: "non-adjacent cycle",
+    addsCycle: true,
+    isApplicableAt: (g, _rng, c) => coordsInGraphBounds(g, add(c, 2)),
+    applyOnGraphAt: (g, rng, { x, y }) => {
+      const [w, h] = graphSize(g);
+      const rw = randomInRange(rng, 3, w - x),
+        rh = randomInRange(rng, 3, h - y);
+
+      const start = getRandomGraphCoordsByFunc(g, (c) =>
+        areCoordsOnRect(c, { x, y }, vec(rw, rh)),
+      );
+      const goal = getRandomGraphCoordsByFunc(
+        g,
+        (c) =>
+          areCoordsOnRect(c, { x, y }, vec(rw, rh)) &&
+          !areCoordsOnRect(c, vec(0), vec(w, h)) &&
+          !is(start, c) &&
+          manhattan(start, c) >= Math.min(rw, rh),
+      );
+      drawBiconnectedDirectionalRect(g, { x, y }, vec(rw, rh), start, goal);
+      graphAddNodeTag(g, start, tags.Start);
+      graphAddNodeTag(g, goal, tags.Goal);
+    },
+    mandatoryFeatures: [
+      {
+        name: "alt paths with hazards",
+        applyFeature: (g) => {
+          const goal = getRandomGraphCoordsByFunc(g, (c) =>
+            graphNodeHasTag(g, c, tags.Goal),
+          );
+          const c1 = getRandomGraphCoordsByFunc(
+            g,
+            ({ x, y }) => g.nodes[x][y].active && adjacent(goal, { x, y }),
+          );
+          const c2 = getRandomGraphCoordsByFunc(
+            g,
+            ({ x, y }) =>
+              g.nodes[x][y].active &&
+              adjacent(goal, { x, y }) &&
+              !is({ x, y }, c1),
+          );
+
+          graphAddNodeTag(g, c1, randomHazard());
+          graphAddNodeTag(g, c2, randomHazard());
+
+          if (randomInRange(rng, 0, 3) == 0)
+            pushNodeContensInRandomDirection(g, goal);
+        },
+      },
+      {
+        name: "two keys",
+        applyFeature: (g) => {
+          const start = getRandomGraphCoordsByFunc(g, (c) =>
+            graphNodeHasTag(g, c, tags.Start),
+          );
+          const goal = getRandomGraphCoordsByFunc(g, (c) =>
+            graphNodeHasTag(g, c, tags.Goal),
+          );
+          const c1 = getRandomGraphCoordsByFunc(
+            g,
+            ({ x, y }) =>
+              g.nodes[x][y].active &&
+              (adjacent(start, { x, y }) || adjacent(goal, { x, y })),
+          );
+          const c2 = getRandomGraphCoordsByFunc(
+            g,
+            ({ x, y }) =>
+              g.nodes[x][y].active &&
+              !is({ x, y }, c1) &&
+              !adjacent(c1, { x, y }) &&
+              (adjacent(start, { x, y }) || adjacent(goal, { x, y })),
+          );
+          graphAddNodeTag(g, c1, tags.HalfKey);
+          graphAddNodeTagPreserveLastId(g, c2, tags.HalfKey);
+          pushNodeContensInRandomDirectionWithEdgeTag(
+            g,
+            goal,
+            tags.BilockedEdge,
+          );
+        },
+      },
+    ],
+  }),
+  rule({
+    name: "adjacent cycle",
+    addsCycle: true,
+    isApplicableAt: (g, _rng, c) => coordsInGraphBounds(g, add(c, 2)),
+    applyOnGraphAt: (g, rng, { x, y }) => {
+      const [w, h] = graphSize(g);
+      const rw = randomInRange(rng, 3, w - x),
+        rh = randomInRange(rng, 3, h - y);
+
+      const start = getRandomGraphCoordsByFunc(g, (c) =>
+        areCoordsOnRect(c, { x, y }, vec(rw, rh)),
+      );
+      const goal = getRandomGraphCoordsByFunc(
+        g,
+        (c) => areCoordsOnRect(c, { x, y }, vec(rw, rh)) && adjacent(start, c),
+      );
+      drawBiconnectedDirectionalRect(g, { x, y }, vec(rw, rh), start, goal);
+      graphAddNodeTag(g, start, tags.Start);
+      graphAddNodeTag(g, goal, tags.Goal);
+    },
+    mandatoryFeatures: [
+      {
+        name: "foresee",
+        applyFeature: (g) => {
+          const start = getRandomGraphCoordsByFunc(g, (c) =>
+            graphNodeHasTag(g, c, tags.Start),
+          );
+          const goal = getRandomGraphCoordsByFunc(g, (c) =>
+            graphNodeHasTag(g, c, tags.Goal),
+          );
+          graphAddEdgeTagByCoords(g, start, goal, tags.WindowEdge);
+        },
+      },
+      {
+        name: "openable shortcut",
+        applyFeature: (g) => {
+          const start = getRandomGraphCoordsByFunc(g, (c) =>
+            graphNodeHasTag(g, c, tags.Start),
+          );
+          const goal = getRandomGraphCoordsByFunc(g, (c) =>
+            graphNodeHasTag(g, c, tags.Goal),
+          );
+          const c1 = getRandomGraphCoordsByFunc(
+            g,
+            (c) =>
+              g.nodes[c.x][c.y].active && !is(start, c) && !adjacent(start, c),
+          );
+          graphAddNodeTag(g, c1, tags.Key);
+          graphAddEdgeTagByCoords(g, start, goal, tags.LockedEdge);
+        },
+      },
+    ],
+  }),
+];
