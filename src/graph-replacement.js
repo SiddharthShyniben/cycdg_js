e66132a30649d51a6385ac6e9a09fbaf67d94258
Siddharth Shyniben <siddharth.muscat@gmail.com>
2024-05-28 11:58:27 +0530

    Create graph replacement class

---
+++ src/graph-replacement.js
@@ -0,0 +1,83 @@
+import { graph, graphGetEnabledNodesCount } from "./graph";
+import { clamp } from "./util";
+
+import allInitalRules from "./grammar/initial-rules";
+import {
+  getRandomApplicableCoordsForRule,
+  isRuleApplicableForGraph,
+} from "./grammar/helper";
+import { rng } from "./rng";
+
+class GraphReplacementApplier {
+  constructor(
+    width = 4,
+    height = 5,
+    {
+      minCycles,
+      maxCycles,
+      desiredFeatures,
+      minFilledPercentage,
+      maxFilledPercentage,
+      maxTeleports,
+    },
+  ) {
+    this.width = clamp(width, 4, 25);
+    this.height = clamp(height, 5, 25);
+
+    this.graph = graph(this.width, this.height);
+
+    this.options = options;
+
+    this.minCycles = clamp(minCycles || 3, 1, 100);
+    this.maxCycles = clamp(maxCycles || 8, 1, 100);
+    this.desiredFeatures = clamp(desiredFeatures || 5, 0, 1000);
+    this.minFilledPercentage = clamp(minFilledPercentage || 65, 25, 100);
+    this.maxFilledPercentage = clamp(maxFilledPercentage || 85, 25, 100);
+    this.maxTeleports = clamp(maxTeleports || 2, 0, 1000);
+
+    if (this.maxCycles < this.minCycles) this.maxCycles = this.minCycles;
+    if (this.maxFilledPercentage < this.minFilledPercentage)
+      this.maxFilledPercentage = this.minFilledPercentage;
+
+    this.reset();
+  }
+
+  reset() {
+    this.desiredFillPercentage = rng.randInRange(
+      this.minFilledPercentage,
+      this.maxFilledPercentage,
+    );
+    this.appliedRules = null;
+    this.appliedRulesCount =
+      this.appliedFeaturesCount =
+      this.teleportsCount =
+      this.cyclesCount =
+      this.enabledNodesCount =
+      this.finalizeDisabledNodesCount =
+        0;
+
+    this.applyRandomInitialRule();
+  }
+
+  applyRandomInitialRule() {
+    const rule = allInitalRules[rng.randInRange(allInitalRules.length)];
+    if (isRuleApplicableForGraph(rule, this.graph)) this.applyInitialRule(rule);
+    else console.error("Rule failed!", rule.name);
+    this.enabledNodesCount = graphGetEnabledNodesCount(this.graph);
+  }
+
+  applyInitialRule(rule) {
+    const c = getRandomApplicableCoordsForRule(rule, graph);
+    rule.applyOnGraphAt(this.graph, c);
+
+    const appliedFeature = rng.fromArr(rule.mandatoryFeatures);
+    appliedFeature.applyFeature(this.graph);
+    if (rule.addsCycle) this.cyclesCount++;
+    this.appliedRulesCount++;
+    this.appliedRules.push({
+      name: rule.name,
+      coords: c,
+      mandatoryFeature: appliedFeature.name,
+    });
+  }
+}
