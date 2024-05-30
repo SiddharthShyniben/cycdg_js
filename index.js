f28149e353d45acd75d8148220034aa98d616a35
Siddharth Shyniben <siddharth.muscat@gmail.com>
2024-05-30 12:34:22 +0530

    Use replacement rules

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 import { GraphReplacementApplier } from "./src/graph-replacement.js";
 import { drawGraph } from "./src/render_graph.js";
+import keypress from "keypress";
 
-const GRA = new GraphReplacementApplier();
+const GRA = new GraphReplacementApplier(10, 5);
 drawGraph(GRA);
+
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on("keypress", function (_, key) {
+  if (key) {
+    if (key.ctrl && key.name == "c") {
+      process.stdin.pause();
+    } else {
+      let done = false;
+
+      if (key.name == "return") {
+        while (!done) done = GRA.applyRandomReplacementRule();
+
+        drawGraph(GRA);
+      } else {
+        done = GRA.applyRandomReplacementRule();
+
+        drawGraph(GRA);
+      }
+    }
+  }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
