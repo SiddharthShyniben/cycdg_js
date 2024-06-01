44e5fe5e488841f9560a0bf0576871893f61f55d
Siddharth Shyniben <siddharth.muscat@gmail.com>
2024-06-01 14:16:22 +0530

    Add support for negative weights

--- src/rng.js
+++ src/rng.js
@@ -28,7 +28,10 @@ export class RNG {
   }
 
   weightedFromArr(items, fn) {
-    const weights = items.map(fn);
+    let weights = items.map(fn);
+
+    const negative = weights.filter((w) => w < 0).sort((a, b) => a - b);
+    if (negative.length > 0) weights = weights.map((w) => w + negative[0] + 1);
     const weighted = [];
 
     for (let i = 0; i < items.length; i++) {
